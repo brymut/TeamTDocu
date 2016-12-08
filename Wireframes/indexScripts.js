7f26124d7e2c2ea35e53c9f9aef6b35c12d1f911
Dafin Kozarev <dafin.kozarev@gmail.com>
2016-12-08 21:50:32 +0000

    Advanced Search now instantly toggles to a more specific search including the filters from the original Avaloq search tool.

--- Wireframes/indexScripts.js
+++ Wireframes/indexScripts.js
@@ -1,9 +1,18 @@
 function onLoad(){
-	/* Searchbox related code */
 	$( "#searchboxtext" ).focus(function() { 
 		$( "#submitbutton" ).hide(); 
 		window.setTimeout(showButton,150);
 	}); 
 	$( "#submitbutton" ).mousedown(function() {$( "#searchboxform" ).submit();});
+	$('.options').buttonset();
+	$('#advancedsearchbutton').click(function() {
+		$( "#searchbox" ).slideUp();
+		$('#advanced').fadeIn();
+		$('#advanced').css("display", "unset");
+	});
+	$('#backtosimple').click(function() {
+		$('#advanced').hide();
+		$( "#searchbox" ).slideDown();
+	});
 }
 function showButton(){ $( "#submitbutton" ).show(); }
