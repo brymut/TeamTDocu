e1d31064e3a64a37f961e0a0bf9fd825fbe08e5f
Dafin Kozarev <dafin.kozarev@gmail.com>
2016-12-09 16:26:22 +0000

    The advanced search form has now been customized with bootstrap. Submit and reset buttons are just placeholders until I come up with a better design.

--- Wireframes/indexScripts.js
+++ Wireframes/indexScripts.js
@@ -3,21 +3,29 @@ function onLoad(){
 		$( "#submitbutton" ).hide(); 
 		window.setTimeout(showButton,150);
 	}); 
+	$( "#submit-adv-b" ).click(function() { 
+		$( "#advancedsearchform" ).submit();
+	});
+	$( "#reset-adv-b" ).click(function() { 
+		$("#advancedsearchform").trigger("reset");
+	});
 	$( "#submitbutton" ).mousedown(function() {$( "#searchboxform" ).submit();});
 	$('.options').buttonset();
+	$('#from-date-input').datepicker({ dateFormat: 'yy-mm-dd'});
+	$('#to-date-input').datepicker({ dateFormat: 'yy-mm-dd'});
 	$('#advancedsearchbutton').click(function() {
 		$("body").css("background-color", "white");
 		$("#searchbox").slideUp();
-		$('#advanced').fadeIn();
+		$('#advanced-container').fadeIn();
 		$('#avaloqinverted').show("slide", { direction: "left" }, 1000);
-		$('.adv').fadeIn();
-		$('#advanced').css("display", "unset");
-		$('.adv').css("display", "unset");
+		$('.adv-s-hide').fadeIn();
+		$('#advanced-container').css("display", "unset");
+		$('.adv-s-hide').css("display", "unset");
 	});
 	$('#backtosimple').click(function() {
 		$("body").css("background-color", "#1F559F");
-		$('#advanced').hide();
-		$('.adv').hide();
+		$('#advanced-container').hide();
+		$('.adv-s-hide').hide();
 		$( "#searchbox" ).slideDown();
 	});
 }
