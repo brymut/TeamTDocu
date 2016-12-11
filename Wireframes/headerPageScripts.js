9cac78ba3de64064a558f9c05bce98f12ebc7e34
Dafin Kozarev <dafin.kozarev@gmail.com>
2016-12-11 12:20:31 +0000

    The development on the results page has progressed. It is currently stored as TemplateHeaderPage.html but they will soon be separated as there is a need for an example header template page to exist. The header has been updated and is now looking modern and sticks to the top when scrolling.

---
+++ Wireframes/headerPageScripts.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+	$(window).scroll(function() {
+		if ($(this).scrollTop() > 1){  
+			$('header').addClass("sticky");
+			$('#avaloqicon').css("display", "");
+			$('#avaloqicon').show();
+			$('#submitbuttonsearch').css("margin-top", "0.7%");
+			$('#searchboxtext').css("margin-top", "0.7%");
+			var logowidth = $('#avaloqlogo').css("width");
+			$('#searchboxtext').css("margin-left", logowidth);
+			$('#avaloqlogo').hide();
+		}
+		else{
+			$('header').removeClass("sticky");
+			$('#avaloqicon').hide();
+			$('#submitbuttonsearch').css("margin-top", "3%");
+			$('#searchboxtext').css("margin-top", "3%");
+			$('#avaloqlogo').show();
+			$('#searchboxtext').css("margin-left", "0px");
+		}
+	});
+});
