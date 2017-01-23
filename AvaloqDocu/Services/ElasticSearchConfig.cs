fa787eca913ad8b99586a2df7b5e8603df69d4d3
brymut <mutaiwork@gmail.com>
2017-01-23 09:48:43 +0000

    ElasticSearch Config
    Added on the Elastic Search client settings that the application will be
    using to interact with ElasticSearch.

---
+++ AvaloqDocu/Services/ElasticSearchConfig.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nest;
+using System.Configuration;
+
+namespace AvaloqDocu.Services
+{
+    public class ElasticSearchConfig
+    {
+        public static string Index
+        {
+            get { return ConfigurationManager.AppSettings["indexName"]; }
+            private set {; }
+        }
+
+        public static string ElasticSearchUrl
+        {
+            get { return ConfigurationManager.AppSettings["elasticsearchUrl"]; }
+            private set {; }
+        }
+
+        // the client that will be used by this application to communicate with Elastic Search
+        public static IElasticClient GetClient()
+        {
+
+            var node = new Uri("http://localhost:9200");
+            var settings = new ConnectionSettings(node)
+                               .DefaultIndex("avaloq_docs");
+
+
+            return new ElasticClient(settings); ;
+
+
+        }
+
+
+    }
+}
