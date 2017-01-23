c1f8400a95ef4b1ded759e96036f330c161ae67e
brymut <mutaiwork@gmail.com>
2017-01-23 09:54:53 +0000

    Document model updated
    Temporary changes to the document model, for development purposes. Will
    clean up when we get a clearer picture of the document format and
    required metadata.

--- AvaloqDocu/Models/Document.cs
+++ AvaloqDocu/Models/Document.cs
@@ -9,10 +9,13 @@ namespace AvaloqDocu.Models
     public class Document
     {
         [Key]
-        public int DocumentId { get; set; }
+        //public int DocumentId { get; set; }
         public string Title { get; set; }
-        public string FilePath { get; set; }
-        public string Description { get; set; }
-        public DateTime UploadDate { get; set; }
+        public string Author { get; set; }
+        //public string FilePath { get; set; }
+        //public string  FileName { get; set; }
+        //public string Description { get; set; }
+        //public DateTime UploadDate { get; set; }
+        public DateTime creation_date { get; set; }
     }
 }
