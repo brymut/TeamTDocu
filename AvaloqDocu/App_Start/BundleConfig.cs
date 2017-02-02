06ae2dd195720298bbd1fc777d128065a12cdc1e
Administrator <2079310R@student.gla.ac.uk>
2017-02-02 16:32:38 +0000

    search functionality linked with interface

--- AvaloqDocu/App_Start/BundleConfig.cs
+++ AvaloqDocu/App_Start/BundleConfig.cs
@@ -10,7 +10,8 @@ namespace AvaloqDocu
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js",
-                        "~/Scripts/knockout-{version}.js"));
+                        "~/Scripts/knockout-{version}.js",
+                        "~/Scripts/moment.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
--- AvaloqDocu/Content/Scripts/search.js
+++ AvaloqDocu/Content/Scripts/search.js
@@ -2,17 +2,52 @@
     var self = this;
     self.query = ko.observable("");
     self.results = ko.observableArray([]);
+    self.total = ko.observable(0);
+    self.page = ko.observable(1);
+    self.pages = ko.observable(1);
+    self.queryTime = ko.observable(0);
 
     self.search = function () {
         $.ajax({
-            url: "/api/search/GetFreeTextSearch?query=" + self.query(),
+            url: "/api/search/GetFreeTextSearch?query=" + self.query() + "&page=" + self.page(),
             type: "GET",
             dataType: "json",
             contentType: "application/json; charset=utf-8",
             success: function (data) {
                 self.results(data.Results);
+                self.total(data.Total);
+                self.queryTime(data.QueryTime);
+                self.setPageNumbers();
             }
         })
+    }
+
+    self.formatDate = function (date) {
+        if (date == null) {
+            return "N/A";
+        }
+        else {
+            return moment(date).format('DD/MM/YYYY');
+        }
+    }
+
+    self.setPageNumbers = function () {
+        self.pages(Math.ceil(self.total() / 10));
+    }
+
+    self.next = function () {
+        self.page(self.page() + 1);
+        self.search();
+    }
+
+    self.prev = function () {
+        self.page(self.page() - 1);
+        self.search();
+    }
+
+    self.updateQuery = function () {
+        window.location.href = "/search?query=" + self.query();
+        self.search();
     }
 }
 
