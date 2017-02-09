045da7a17bfd855b9de055e2df4bf6be6a39f507
KyleBurns <2137712b@student.gla.ac.uk>
2017-02-09 09:57:24 +0000

    Updated document model

--- AvaloqDocu/Models/Document.cs
+++ AvaloqDocu/Models/Document.cs
@@ -12,7 +12,7 @@ namespace AvaloqDocu.Models
         public int DocumentId { get; set; }
         public string Title { get; set; }
         public string Author { get; set; }
-        public string FilePath { get; set; }
+        public FilePath FilePath { get; set; }
         public string Description { get; set; }
         public DateTime UploadDate { get; set; }
         public DateTime CreationDate { get; set; }
