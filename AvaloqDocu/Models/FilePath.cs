fd8cc0aa3ff075e5b059abdaab9040e514c12a0c
KyleBurns <2137712b@student.gla.ac.uk>
2017-02-09 09:53:23 +0000

    Added uploader services/models to master branch

---
+++ AvaloqDocu/Models/FilePath.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AvaloqDocu.Models
+{
+    using System.ComponentModel.DataAnnotations;
+    public class FilePath
+    {
+        public int FilePathId { get; set; }
+        public string FileName { get; set; }
+    }
+}
---
+++ AvaloqDocu/Services/UploadService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace AvaloqDocu.Services
+{
+    public class UploadService
+    {
+        [HttpPost]
+        public void Create(HttpPostedFileBase upload)
+        {
+            using (var dc = new DocuContext())
+            {
+                var document = new Models.Document { };
+                if (upload != null && upload.ContentLength > 0)
+                {
+                    document.DocumentId = 0;
+                    document.Author = "null";
+                    document.Description = "null";
+                    document.Title = "null";
+                    var filepath = new Models.FilePath
+                    {
+                        FileName = System.IO.Path.GetFileName(upload.FileName),
+                    };
+                    document.FilePath = filepath;
+                }
+                dc.Documents.Add(document);
+                dc.SaveChanges();
+            }
+        }
+    }
+}
