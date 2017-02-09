1b2d1502856c4e1b7e8721d416252e9c14faee50
KyleBurns <2137712b@student.gla.ac.uk>
2017-02-09 11:54:58 +0000

    Added search controller test class

---
+++ AvaloqDocu.Tests/Controllers/SearchControllerTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AvaloqDocu;
+using AvaloqDocu.Controllers;
+
+namespace AvaloqDocu.Tests.Controllers
+{
+    [TestClass]
+    public class SearchControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+            // Arrange
+            SearchController controller = new SearchController();
+
+            // Act
+            ViewResult result = controller.Index("finance") as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
