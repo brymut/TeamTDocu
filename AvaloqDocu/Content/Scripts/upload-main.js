a20f1f760b22d4a55b957a3067479640ad39f5a6
Dafin Kozarev <dafin.kozarev@gmail.com>
2017-02-24 14:07:18 +0000

    Upload back-end now works with current interface.

--- AvaloqDocu/Content/Scripts/upload-main.js
+++ AvaloqDocu/Content/Scripts/upload-main.js
@@ -18,7 +18,7 @@ $(function () {
     $('#fileupload').fileupload({
         // Uncomment the following to send cross-domain cookies:
         //xhrFields: {withCredentials: true},
-        url: 'http://jquery-file-upload.appspot.com/'
+        url: 'http://localhost:64376/Upload/UploadFiles'
     });
 
     // Enable iframe cross-domain access via redirect option:
