89b52d4bd0830cbff7959bf9644757767291ee42
KyleBurns <2137712b@student.gla.ac.uk>
2017-03-08 16:16:20 +0000

    Added framework to test service methods - ticket #38

---
+++ AvaloqDocu.Tests/Services/PackageServiceTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AvaloqDocu.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AvaloqDocu.Services.Tests
+{
+    [TestClass()]
+    public class PackageServiceTests
+    {
+        [TestMethod()]
+        public void AddPackageTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void AddDocumentToPackageTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
